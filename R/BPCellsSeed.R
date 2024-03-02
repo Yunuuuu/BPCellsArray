a2cf3e4291440ca4bb9291444df2a666c10e00af
yun <yunyunpp96@outlook.com>
2024-03-02 19:19:58 +0800

    fix incompatible storage axis

--- R/BPCellsSeed.R
+++ R/BPCellsSeed.R
@@ -54,19 +54,19 @@ methods::setMethod("BPCellsSeed", "dgCMatrix", function(x) {
 #' @export
 #' @rdname BPCellsSeed
 methods::setMethod("BPCellsSeed", "ANY", function(x) {
-    # prepare for message usage
     # catch the argumeent passed into `BPCellsSeed` function
-    arg <- substitute(x, env = sys.frame(-2L)) # nolint
-    # Get current environemnt
-    current_env <- environment()
-    x <- tryCatch(
+    x <- coerce_into_dgCMatrix(x, arg = substitute(x, env = sys.frame(-2L)))
+    methods::callGeneric()
+})
+
+coerce_into_dgCMatrix <- function(x, arg = rlang::caller_arg(x), call = rlang::caller_env()) {
+    rlang::try_fetch(
         methods::as(x, "dgCMatrix"),
         error = function(cnd) {
             cli::cli_abort(
                 "{.arg {arg}} must be a matrix-like object which can be coerced into {.cls dgCMatrix}",
-                call = current_env
+                call = call
             )
         }
     )
-    methods::callGeneric()
-})
+}
--- R/Method-Subassign.R
+++ R/Method-Subassign.R
@@ -1,13 +1,61 @@
 ################    BPCellsMatrix Methods    ##################
 #' @return
 #' - `[<-`: A `BPCellsMatrix` object.
+#' @inheritDotParams BPCells::transpose_storage_order -matrix
+#' @export
+#' @rdname BPCellsMatrix-class
+methods::setMethod(
+    "[<-", c("BPCellsMatrix", "ANY", "ANY", "BPCellsMatrix"),
+    function(x, i, j, ..., value) {
+        # coerce `value` into `IterableMatrix`
+        value <- to_BPCells(value@seed)
+        # Recall `x = BPCellsMatrix` and `value = IterableMatrix` method
+        methods::callGeneric()
+    }
+)
+
+#' @export
+#' @rdname BPCellsMatrix-class
+methods::setMethod(
+    "[<-", c("BPCellsMatrix", "ANY", "ANY", "IterableMatrix"),
+    array_call_BPCells_method(
+        x = , i = , j = , ... = , value = ,
+        before2 = expression(
+            # keep consistent storage mode
+            x_mode <- storage_mode(x),
+            value_mode <- storage_mode(value),
+            if (x_mode != value_mode) {
+                cli::cli_warn(incompatible_mode_msg("x", "value", x_mode))
+                value <- BPCells::convert_matrix_type(
+                    matrix = value, type = x_mode
+                )
+            },
+            # keep consistent storage axis
+            if (x@transpose != value@transpose) {
+                cli::cli_warn(c(
+                    "!" = c_msg(
+                        "Incompatible storage axis between",
+                        "{.arg x} and {.arg value}"
+                    ),
+                    i = "transposing the storage axis of {.arg value}"
+                ))
+                value <- BPCells::transpose_storage_order(matrix = value, ...)
+            }
+        )
+    )
+)
+
 #' @export
 #' @rdname BPCellsMatrix-class
 methods::setMethod(
     "[<-", c("BPCellsMatrix", "ANY", "ANY", "ANY"),
-    function(x, i, j, ..., value) {
-        value <- BPCellsSeed(value)
-        # Recall `x = BPCellsMatrix` and `value = IterableMatrix` method
+    function(x, i, j, value) {
+        # coerce `value` into `dgCMatrix`
+        value <- coerce_into_dgCMatrix(value,
+            # this will capture the argument passed into `[<-` generic function
+            arg = substitute(value, env = sys.frame(-2L))
+        )
+        # Recall `x = BPCellsMatrix` and `value = dgCMatrix` method
         methods::callGeneric()
     }
 )
@@ -16,45 +64,24 @@ methods::setMethod(
 #' @export
 #' @rdname internal-methods
 methods::setMethod(
-    "[<-", c("BPCellsMatrix", "ANY", "ANY", "BPCellsMatrix"),
-    function(x, i, j, ..., value) {
-        value <- value@seed
-        # Recall `x = BPCellsMatrix` and `value = IterableMatrix` method
-        methods::callGeneric()
-    }
-)
-
-#' @export
-#' @rdname internal-methods
-methods::setMethod(
     "[<-", c("BPCellsMatrix", "ANY", "ANY", "dgCMatrix"),
-    function(x, i, j, ..., value) {
-        if (x@transpose) {
-            value <- t(BPCellsSeed(t(value)))
-        } else {
-            value <- BPCellsSeed(value)
-        }
-        # Recall `x = BPCellsMatrix` and `value = IterableMatrix` method
-        methods::callGeneric()
-    }
-)
-
-#' @export
-#' @rdname internal-methods
-methods::setMethod(
-    "[<-", c("BPCellsMatrix", "ANY", "ANY", "IterableMatrix"),
-    function(x, i, j, ..., value) {
-        seedform <- x@SeedForm
-        x <- to_BPCells(x@seed)
-        x_mode <- storage_mode(x)
-        value_mode <- storage_mode(value)
-        if (x_mode != value_mode) {
-            cli::cli_warn("Convert {.arg value} into {.field uint32_t} mode")
-            value <- BPCells::convert_matrix_type(matrix = value, type = x_mode)
-        }
-        # Recall `x = IterableMatrix` and `value = IterableMatrix` method
-        with_seedform(seedform, DelayedArray(methods::callGeneric()))
-    }
+    array_call_BPCells_method(
+        x = , i = , j = , value = ,
+        before2 = expression(
+            x_mode <- storage_mode(x),
+            if (x@transpose) {
+                value <- t(BPCellsSeed(t(value)))
+            } else {
+                value <- BPCellsSeed(value)
+            },
+            if (x_mode != "double") {
+                cli::cli_warn(incompatible_mode_msg("x", "value", x_mode))
+                value <- BPCells::convert_matrix_type(
+                    matrix = value, type = x_mode
+                )
+            }
+        )
+    )
 )
 
 # respect matrix storage.mode
@@ -62,29 +89,36 @@ methods::setMethod(
 #' @rdname internal-methods
 methods::setMethod(
     "[<-", c("BPCellsMatrix", "ANY", "ANY", "matrix"),
-    function(x, i, j, ..., value) {
-        seedform <- x@SeedForm
-        x <- to_BPCells(x@seed)
-        x_mode <- storage_mode(x)
-        value_mode <- storage_mode(value)
-        value <- methods::as(value, "dgCMatrix")
-        if (x@transpose) {
-            value <- t(BPCellsSeed(t(value)))
-        } else {
-            value <- BPCellsSeed(value)
-        }
-        if (x_mode == "uint32_t" && value_mode != "uint32_t") {
-            cli::cli_warn("Convert {.arg value} into {.field uint32_t} mode")
-            value <- BPCells::convert_matrix_type(
-                matrix = value, type = "uint32_t"
-            )
-        } else if (x_mode != "uint32_t" && value_mode == "uint32_t") {
-            cli::cli_warn("Convert {.arg value} into {.field {x_mode}} mode")
-            value <- BPCells::convert_matrix_type(matrix = value, type = x_mode)
-        } else {
-            value <- BPCells::convert_matrix_type(matrix = value, type = x_mode)
-        }
-        # Recall `x = IterableMatrix` and `value = IterableMatrix` method
-        with_seedform(seedform, DelayedArray(methods::callGeneric()))
-    }
+    array_call_BPCells_method(
+        x = , i = , j = , value = ,
+        before2 = expression(
+            x_mode <- storage_mode(x),
+            value_mode <- storage_mode(value),
+            # convert `value` into a `IterableMatrix` object
+            # I don't need BPCellsSeed to take care of the storage mode of
+            # `value`, since we'll do it special
+            value <- methods::as(value, "dgCMatrix"),
+            if (x@transpose) {
+                value <- t(BPCellsSeed(t(value)))
+            } else {
+                value <- BPCellsSeed(value)
+            },
+            # convert the storage mode of `value` when necessary
+            if (x_mode == "uint32_t" && value_mode != "uint32_t") {
+                cli::cli_warn(incompatible_mode_msg("x", "value", x_mode))
+                value <- BPCells::convert_matrix_type(
+                    matrix = value, type = "uint32_t"
+                )
+            } else if (x_mode != "uint32_t" && value_mode == "uint32_t") {
+                cli::cli_warn(incompatible_mode_msg("x", "value", x_mode))
+                value <- BPCells::convert_matrix_type(
+                    matrix = value, type = x_mode
+                )
+            } else {
+                value <- BPCells::convert_matrix_type(
+                    matrix = value, type = x_mode
+                )
+            }
+        )
+    )
 )
